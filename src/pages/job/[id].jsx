b1a69fbab8363e655b74e5aa2a83548536a1e8b0
AshokParmar24 <ashokparmar72038@gmail.com>
2025-06-25 15:42:44 +0530

    change

--- src/pages/job/[id].jsx
+++ src/pages/job/[id].jsx
@@ -3,25 +3,40 @@
 import { set, useForm } from "react-hook-form";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { AiOutlineClose, AiOutlineFilePdf } from "react-icons/ai";
+import {
+  AiOutlineClose,
+  AiOutlineFilePdf,
+  AiOutlineArrowLeft,
+} from "react-icons/ai";
 import { FiUpload } from "react-icons/fi";
 
 const ApplyForJob = () => {
   const router = useRouter();
   const { id } = router.query;
-  const [file, setfile] = useState(null);
+  const [file, setFile] = useState(null);
   const [err, setErr] = useState(false);
 
   const {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm();
 
   const onSubmit = (data) => {
+    const payload = {
+      ...data,
+      file: file,
+      jobApplyId: id,
+    };
     if (!file) {
       setErr(true);
     } else {
+      const applyJob = JSON.stringify(localStorage.getItem("applyJob"));
+      const newApply = [...applyJob, payload];
+      localStorage.setItem("applyJob", JSON.stringify(newApply));
+      reset();
+      setFile(null);
       router.push("/");
     }
   };
@@ -36,9 +51,19 @@ const ApplyForJob = () => {
 
   return (
     <div className="max-w-xl mx-auto p-6 bg-white rounded-lg shadow mt-10">
-      <h2 className="text-2xl font-bold mb-6 text-gray-800">
-        Apply for this Job
-      </h2>
+      <div className="flex justify-between">
+        <h2 className="text-2xl font-bold mb-6 text-gray-800">
+          Apply for this Job
+        </h2>
+        <button
+          type="button"
+          onClick={() => router.push("/")}
+          className="text-gray-500 hover:text-gray-700  hover:text-gray-300 cursor-pointer bg-gray-100 rounded-full h-10 w-10 flex justify-center"
+          title="Go back"
+        >
+          <AiOutlineArrowLeft size={24} className="mt-2" />
+        </button>
+      </div>
 
       <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
         <div>
@@ -69,7 +94,7 @@ const ApplyForJob = () => {
         {file ? (
           <div className="relative w-full flex flex-col items-center gap-2 border rounded-md p-3">
             <button
-              onClick={() => setfile(null)}
+              onClick={() => setFile(null)}
               className="absolute top-2 right-2 text-gray-500 hover:text-red-600"
               title="Remove file"
             >
@@ -96,7 +121,7 @@ const ApplyForJob = () => {
                         }`}
               onChange={(e) => {
                 const file = e.target.files[0];
-                setfile(file || null);
+                setFile(file || null);
               }}
             />
 
@@ -105,7 +130,7 @@ const ApplyForJob = () => {
                 <div className="flex flex-col items-center">
                   <FiUpload className="text-gray-400 w-12 h-12 opacity-50" />
 
-                  <span className="text-sm text-gray-500 mt-2">
+                  <span className="text-sm text-gray-500 mt-2 cursur-pointer">
                     No resume uploaded
                   </span>
                 </div>

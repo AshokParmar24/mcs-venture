235ab3f8e4052e161f78c553b9bbdfbd55525969
AshokParmar24 <ashokparmar72038@gmail.com>
2025-06-25 14:03:11 +0530

    cha nge

--- src/services/api.js
+++ src/services/api.js
@@ -3,3 +3,7 @@ import axiosInstance from "./axios";
 export const getJobsList = (payload) => {
   return axiosInstance.get("jobs", payload);
 };
+
+export const getJobsDetails = (id, payload) => {
+  return axiosInstance.get(`jobs/${id}`, payload);
+};
